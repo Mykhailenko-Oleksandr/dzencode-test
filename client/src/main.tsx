110c43b8d5da1e1404d4aa2f05f9ad42b8565163
Sapper <saniamykhailenko@gmail.com>
2026-06-16 20:32:51 +0300

    add store's provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
---
+++ client/src/store/index.ts
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import appReducer from "./appSlice";
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from "react-redux";
+
+export const store = configureStore({
+  reducer: {
+    app: appReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

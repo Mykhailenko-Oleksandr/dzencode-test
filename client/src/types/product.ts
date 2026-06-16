3cf1e40a129e6224974f81be1b6d5a63e9e908eb
Sapper <saniamykhailenko@gmail.com>
2026-06-16 19:55:50 +0300

    add types and favicon

---
+++ client/src/types/product.ts
@@ -0,0 +1,24 @@
+export interface Product {
+  id: number;
+  serialNumber: number;
+  isNew: number;
+  photo: string;
+  title: string;
+  type: string;
+  specification: string;
+  guarantee: Guarantee;
+  price: Price[];
+  order: number;
+  date: string;
+}
+
+interface Price {
+  value: number;
+  symbol: string;
+  isDefault: number;
+}
+
+interface Guarantee {
+  start: string;
+  end: string;
+}
